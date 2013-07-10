e29cf9016da2e9d52d0df40512ea67b265a4713f
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-10 16:58:06 -0700

    DONE

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,21 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  validates :email, format: { with: /\b[a-zA-Z0-9._%-]+@[a-zA-Z0-9-]+.[a-zA-Z]{2,}\b/, 
+     message: "Must be a valid email."}
+
+  validates_uniqueness_of :email
+  validates :age, numericality: {greater_than: 5} 
+  validates :phone, format: { with: /.*\d{3}.+\d{3}.+\d{4}.*/ }
+
+  def name
+    self.first_name + ' ' + self.last_name
+  end
+
+  def age
+    # Date.today.year - self.birthday.year
+    Date.today.year - self.birthday.year - ((Date.today.month > self.birthday.month || (Date.today.month == self.birthday.month && Date.today.day >= self.birthday.day)) ? 0 : 1)
+  end
+
+
 end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,22 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.string :email
+      t.string :phone
+      t.date :birthday
+    end
   end
 end
+
+
+
+# it "should have the right columns and types" do
+#     expected = {
+#       :integer => ["id"],
+#       :string => ["first_name", "last_name", "gender", "email", "phone"],
+#       :date => ["birthday"]
+#     }
